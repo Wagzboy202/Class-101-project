246955a7e592c0be7cb38aac87f959cd906ffedd
Wagzboy202 <107010546+Wagzboy202@users.noreply.github.com>
2023-06-11 20:03:27 -0500

    Add files via upload

---
+++ roll-a-dice.py
@@ -0,0 +1,48 @@
+import random
+
+response = "y"
+
+while response == "y":
+    no = random.randint(1, 6)
+    
+    if no == 1:
+        print("---------")
+        print("|       |")
+        print("|   *   |")
+        print("|       |")
+        print("---------")
+    elif no == 2:
+        print("---------")
+        print("| *     |")
+        print("|       |")
+        print("|     * |")
+        print("---------")
+    elif no == 3:
+        print("---------")
+        print("| *     |")
+        print("|   *   |")
+        print("|     * |")
+        print("---------")
+    elif no == 4:
+        print("---------")
+        print("| *   * |")
+        print("|       |")
+        print("| *   * |")
+        print("---------")
+    elif no == 5:
+        print("---------")
+        print("| *   * |")
+        print("|   *   |")
+        print("| *   * |")
+        print("---------")
+    else:
+        print("---------")
+        print("| *   * |")
+        print("| *   * |")
+        print("| *   * |")
+        print("---------")
+    
+    response = input("Do you want to roll the dice again? (y/n): ")
+    
+    if response != "y":
+        break
